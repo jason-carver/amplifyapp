9245c0209a6a18886425dec4cad68018f16dc541
jason-carver <jbrittcarver@hotmail.com>
2020-10-27 14:38:13 -0500

    personalizing V2

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <h1>Hello from V2</h1>
+       <p>powered by the great mind of Jason Carver</p>
       </header>
     </div>
   );
